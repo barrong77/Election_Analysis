56908b2c7727f660ccfca004bf2974d468e4efb7
barrong77 <108476566+barrong77@users.noreply.github.com>
2022-08-23 13:30:48 -0600

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -6,17 +6,38 @@
 #5. The winner of the election based on popular vote.
 # Assign a variable for the file to load and the path.
 #Create a filename variable to a direct or indirect path to the file.
-file_to_load = 'Resources/election_results.csv'
-election_data = open(file_to_load, 'r')
-election_data.close()
-with open(file_to_load) as election_data:
-    print(election_data)
+#Add our dependencies.
 import csv
 import os
+from wsgiref import headers
+#Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
+#Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+#1. Initialize a total vote counter.
+total_votes = 0
+#Candidate Options
+candidate_options = []
+#1. Declare the empty dictionary.
+candidate_votes = {}
+#Open the election results and read the file.
 with open(file_to_load) as election_data:
-#To do: read and analyze the data here
-#Read the file object with the reader function.
-  headers = next(file_reader)
-print(headers)
+    file_reader = csv.reader(election_data)
+    #Read the header row.
+    headers = next(file_reader)
+    #Print each row in the CSV file.
+    for row in file_reader:
+        #2. Add to the total vote count.
+        total_votes+=1
+        #Print the candidate name from each row.
+        candidate_name = row[2]
+        #If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+        #Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+        #2. Begin tracking that candidates vote count.
+            candidate_votes[candidate_name]=0
+        #Add a vote to the candidate count.
+        candidate_votes[candidate_name]+=1
+#3. Print the total votes.
+print(candidate_votes)
