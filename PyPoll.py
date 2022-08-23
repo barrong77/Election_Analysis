447f59246d3e915300a1deaf3af6593f735cfcc7
barrong77 <108476566+barrong77@users.noreply.github.com>
2022-08-23 14:34:41 -0600

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -20,6 +20,10 @@ total_votes = 0
 candidate_options = []
 #1. Declare the empty dictionary.
 candidate_votes = {}
+#Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 #Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -39,5 +43,20 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name]=0
         #Add a vote to the candidate count.
         candidate_votes[candidate_name]+=1
+#Determine the percentage of votes for each candidate by looping through the counts.
+#1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    #2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    #3. Calculcate the percentage of votes.
+    vote_percentage = float(votes)/float(total_votes) * 100
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    if (votes>winning_count) and (vote_percentage>winning_percentage):
+        winning_count = votes
+        winning_percentage=vote_percentage
+        winning_candidate=candidate_name
+    #4. Print the candidate name and percentage of votes.
+    print(f"{candidate_name}:received {vote_percentage}% of the vote.")
+
 #3. Print the total votes.
 print(candidate_votes)
